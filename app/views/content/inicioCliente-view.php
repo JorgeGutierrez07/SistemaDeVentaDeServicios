3d0ad4384adedc2b588cf961f2f1a2feb28e77d4
Geovanni Hernandez <geo.hdez.and@gmail.com>
2024-10-31 23:02:54 -0600

    pantalla-ClienteFin

--- app/views/content/inicioCliente-view.php
+++ app/views/content/inicioCliente-view.php
@@ -1,18 +1,21 @@
-<body class="d-flex flex-column h-100">
+
+<body class="d-flex flex-column vh-100">
     <header class="bg-dark text-white p-3">
         <div class="d-flex justify-content-between align-items-center">
-            <h1 class="h5">Mi Aplicación</h1>
-            <button class="btn btn-outline-light">Cerrar Sesión</button>
+		<svg class=" text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" viewBox="0 0 24 24">
+				<path d="M21.972 11.517a.527.527 0 0 0-1.034-.105 1.377 1.377 0 0 1-1.324 1.01 1.467 1.467 0 0 1-1.4-1.009.526.526 0 0 0-1.015 0 1.467 1.467 0 0 1-2.737.143l-.049-.204.021-.146V9.369h2.304a2.632 2.632 0 0 0 2.631-2.632 2.678 2.678 0 0 0-2.654-2.632l-.526.022-.13-.369A2.632 2.632 0 0 0 13.579 2c-.461 0-.915.124-1.313.358L12 2.513l-.266-.155A2.603 2.603 0 0 0 10.422 2a2.632 2.632 0 0 0-2.483 1.759l-.13.37-.518-.024a2.681 2.681 0 0 0-2.66 2.632A2.632 2.632 0 0 0 7.264 9.37H9.61v1.887l-.007.09-.028.08a1.328 1.328 0 0 1-1.301.996 1.632 1.632 0 0 1-1.502-1.024.526.526 0 0 0-1.01.013 1.474 1.474 0 0 1-1.404 1.01 1.381 1.381 0 0 1-1.325-1.01.547.547 0 0 0-.569-.382h-.008a.526.526 0 0 0-.456.526v.446a10.012 10.012 0 0 0 10 10 9.904 9.904 0 0 0 7.067-2.94A10.019 10.019 0 0 0 22 11.966l-.028-.449ZM8.316 15.685a1.053 1.053 0 1 1 2.105 0 1.053 1.053 0 0 1-2.105 0Zm1.58 3.684a2.105 2.105 0 0 1 4.21 0h-4.21Zm4.736-2.631a1.052 1.052 0 1 1 0-2.105 1.052 1.052 0 0 1 0 2.105Z"/>
+				</svg>
+            <button class="btn btn-outline-light btn-lg px-5">Cerrar Sesion</button>
         </div>
     </header>
 
-    <div class="container mt-4 content">
-        <div class="row h-100">
-            <div class="col-12">
+    <div class="container mt-4 content d-flex justify-content-center align-items-center vh-100">
+        <div class="row w-100 h-60">
+            <div class="col-12 ">
                 <div class="card text-center bg-light h-100">
                     <div class="card-body d-flex flex-column justify-content-center align-items-center h-100">
                         <h2 class="text-primary">¡Bienvenido, Usuario!</h2>
-                        <p>Este es un espacio para mostrar información relevante.</p>
+                        <p>Aqui te mostraremos informacion importante para ti.</p>
                     </div>
                 </div>
             </div>
--- app/views/content/registroUsuario-view.php
+++ app/views/content/registroUsuario-view.php
@@ -1,53 +1,61 @@
-<section class="vh-100 gradient-custom">
-    <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-8">
-                <div class="card bg-dark text-white" style="border-radius: 1rem;">
-                    <div class="card-body p-5 text-center">
-						<h2 class="fw-bold mb-2 text-uppercase">Registro usuario</h2>
-						<p class="text-white-50 mb-2">Por favor ingresa tus datos para crear tu perfil</p>
-						<form  action="" method="POST">
-							<div class="d-flex gap-3">
-								<div data-mdb-input-init class="form-outline form-white w-100 mb-2">
-									<input type="text" name="nombre" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required class="form-control form-control-lg" />
-									<label class="form-label" for="nombre">Nombre</label>
-								</div>
-								
-								<div data-mdb-input-init class="form-outline form-white w-100 mb-2">
-									<input type="password" name="apellido" pattern="^[a-zA-Z0-9]{6,}$" maxlength="100" required class="form-control form-control-lg" />
-									<label class="form-label" for="apellido">Apellido</label>
-								</div>
-	
-							</div>
-							<div class="d-flex gap-3">
-								<div data-mdb-input-init class="form-outline w-100 form-white mb-2">
-									<input type="email" name="correo" pattern="^[a-zA-Z0-9]{6,}$" maxlength="100" required class="form-control form-control-lg" />
-									<label class="form-label" for="correo">Correo electrónico</label>
-								</div>
-								<div data-mdb-input-init class="form-outline w-100 form-white mb-2">
-									<input type="text" name="usuario" pattern="^[a-zA-Z0-9]{6,}$" maxlength="100" required class="form-control form-control-lg" />
-									<label class="form-label" for="usuario">Usuario</label>
-								</div>
-							</div>
-
-							<div class="d-flex justify-content-center">
-								<div data-mdb-input-init class="form-outline  form-white mb-2">
-									<input type="password" name="clave" pattern="^[a-zA-Z0-9]{6,}$" maxlength="100" required class="form-control form-control-lg" />
-									<label class="form-label" for="clave">Clave</label>
-								</div>
-							</div>
-								
-							<div>
-								<button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5 mb-4" type="submit">Registrar</button>
-							</div>
-
-							<div>
-								<p class="mb-0">¿Ya tienes cuenta? <a href="/SistemaDeVentaDeServicios/login" class="text-white fw-bold"> Inicia sésion</a></p>
-							</div>
-						</form>
-					</div>
-                </div>
-            </div>
+<body class="d-flex flex-column vh-100">
+	<header class="bg-dark text-white p-3">
+        <div class="d-flex justify-content-between align-items-center">
+        
+				<svg class=" text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" viewBox="0 0 24 24">
+				<path d="M21.972 11.517a.527.527 0 0 0-1.034-.105 1.377 1.377 0 0 1-1.324 1.01 1.467 1.467 0 0 1-1.4-1.009.526.526 0 0 0-1.015 0 1.467 1.467 0 0 1-2.737.143l-.049-.204.021-.146V9.369h2.304a2.632 2.632 0 0 0 2.631-2.632 2.678 2.678 0 0 0-2.654-2.632l-.526.022-.13-.369A2.632 2.632 0 0 0 13.579 2c-.461 0-.915.124-1.313.358L12 2.513l-.266-.155A2.603 2.603 0 0 0 10.422 2a2.632 2.632 0 0 0-2.483 1.759l-.13.37-.518-.024a2.681 2.681 0 0 0-2.66 2.632A2.632 2.632 0 0 0 7.264 9.37H9.61v1.887l-.007.09-.028.08a1.328 1.328 0 0 1-1.301.996 1.632 1.632 0 0 1-1.502-1.024.526.526 0 0 0-1.01.013 1.474 1.474 0 0 1-1.404 1.01 1.381 1.381 0 0 1-1.325-1.01.547.547 0 0 0-.569-.382h-.008a.526.526 0 0 0-.456.526v.446a10.012 10.012 0 0 0 10 10 9.904 9.904 0 0 0 7.067-2.94A10.019 10.019 0 0 0 22 11.966l-.028-.449ZM8.316 15.685a1.053 1.053 0 1 1 2.105 0 1.053 1.053 0 0 1-2.105 0Zm1.58 3.684a2.105 2.105 0 0 1 4.21 0h-4.21Zm4.736-2.631a1.052 1.052 0 1 1 0-2.105 1.052 1.052 0 0 1 0 2.105Z"/>
+				</svg>
+			
+            <button class="btn btn-outline-light">Inicio</button>
         </div>
-    </div>
-</section>
+    </header>	
+	<div class="container d-flex justify-content-center align-items-center vh-100">
+		<div class="col-xl-8">
+			<div class="card bg-dark text-white" style="border-radius: 1rem;">
+				<div class="card-body p-5 text-center">
+					<h2 class="fw-bold mb-2 text-uppercase">Registro usuario</h2>
+					<p class="text-white-50 mb-2">Por favor ingresa tus datos para crear tu perfil</p>
+					<form  action="" method="POST">
+						<div class="d-flex gap-3">
+							<div data-mdb-input-init class="form-outline form-white w-100 mb-2">
+								<input type="text" name="nombre" pattern="[a-zA-Z0-9]{4,20}" maxlength="20" required class="form-control form-control-lg" />
+								<label class="form-label" for="nombre">Nombre</label>
+							</div>
+							
+							<div data-mdb-input-init class="form-outline form-white w-100 mb-2">
+								<input type="password" name="apellido" pattern="^[a-zA-Z0-9]{6,}$" maxlength="100" required class="form-control form-control-lg" />
+								<label class="form-label" for="apellido">Apellido</label>
+							</div>
+
+						</div>
+						<div class="d-flex gap-3">
+							<div data-mdb-input-init class="form-outline w-100 form-white mb-2">
+								<input type="email" name="correo" pattern="^[a-zA-Z0-9]{6,}$" maxlength="100" required class="form-control form-control-lg" />
+								<label class="form-label" for="correo">Correo electrónico</label>
+							</div>
+							<div data-mdb-input-init class="form-outline w-100 form-white mb-2">
+								<input type="text" name="usuario" pattern="^[a-zA-Z0-9]{6,}$" maxlength="100" required class="form-control form-control-lg" />
+								<label class="form-label" for="usuario">Usuario</label>
+							</div>
+						</div>
+
+						<div class="d-flex justify-content-center">
+							<div data-mdb-input-init class="form-outline  form-white mb-2">
+								<input type="password" name="clave" pattern="^[a-zA-Z0-9]{6,}$" maxlength="100" required class="form-control form-control-lg" />
+								<label class="form-label" for="clave">Clave</label>
+							</div>
+						</div>
+							
+						<div>
+							<button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5 mb-4" type="submit">Registrar</button>
+						</div>
+
+						<div>
+							<p class="mb-0">¿Ya tienes cuenta? <a href="/SistemaDeVentaDeServicios/login" class="text-white fw-bold"> Inicia sésion</a></p>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
