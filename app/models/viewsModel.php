2155ebae92b519695f95c7954a8f4900b6b9ba3b
Bruno <brunoandre27jul@gmail.com>
2024-11-03 15:10:53 -0600

    Ayuda en el problema

--- app/models/viewsModel.php
+++ app/models/viewsModel.php
@@ -7,7 +7,7 @@ class viewsModel
     protected function obtenerVistasModelo($vista)
     {
         // Lista blanca de vistas permitidas
-        $listaBlanca = ["registro"];
+        $listaBlanca = ["cargarFactura", "inicio", "inicoCliente", "login", "registroCliente", "registroProveedor", "registroUsuario"];
         // Comprobación si la vista está en la lista blanca
         if (in_array($vista, $listaBlanca)) {
             // Verificar si el archivo de vista existe
