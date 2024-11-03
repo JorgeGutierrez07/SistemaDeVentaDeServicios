a2c38c7eabe29b578869bf22a8bf4f0dcf303d38
Luis <luis.diaz.navarro.210@gmail.com>
2024-11-03 00:12:14 -0600

    Creacion de alertas

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -4,5 +4,31 @@
 	use app\models\mainModel;
 
 	class userController extends mainModel{ 
+
+		#Controlador para registrar proveedor
+		public function registrarProveedorControlador(){
+			$nombre = $this->limpiarCadena($_POST['nombre']);
+			$apellidos = $this->limpiarCadena($_POST['apellidos']);
+			$email = $this->limpiarCadena($_POST['email']);
+			$nEmpresa = $this->limpiarCadena($_POST['nombre_empresa']);
+			$nUsuario = $this->limpiarCadena($_POST['nombre_usuario']);
+			$clave = $this->limpiarCadena($_POST['clave']);
+			$curp = $this->limpiarCadena($_POST['curp']);
+			$rfc = $this->limpiarCadena($_POST['rfc']);
+			$acta = $this->limpiarCadena($_POST['acta']);
+
+			if($nombre == "" || $apellidos == "" || $email == "" ||
+			 $nEmpresa == "" || $nUsuario == "" || $clave == "" ||
+			 $curp == "" || $rfc == "" || $acta == "") {
+				$alerta = [
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrio un error inesperado",
+					"texto"=>"No has llenado todos los campos solicitados",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+			 }
+
+		}
         
     }
--- app/views/js/ajax.js
+++ app/views/js/ajax.js
@@ -25,10 +25,35 @@ formularios_ajax.forEach(formulario => {
 function alerta_ajax(alerta){
         if(alerta.tipo == "simple"){
             Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: "Something went wrong!",
-                footer: '<a href="#">Why do I have this issue?</a>'
+                icon: alerta.icono,
+                title: alerta.tituto,
+                text: alerta.texto,
+                confirmButtonText: "Aceptar",
+                confirmButtonColor: "#3085d6"
               });
+        } else if ( alerta.tipo == "fail"){
+          Swal.fire({
+            title: alerta.tituto,
+            text: alerta.texto,
+            icon: alerta.icono,
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Aceptar"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              location.reload();
+            }
+          });
+        } else if(alerta.tipo == "success"){
+          Swal.fire({
+            title: alerta.tituto,
+            text: alerta.texto,
+            icon: alerta.icono,
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Aceptar"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.location.href = alerta.url;
+            }
+          });
         }
 }
